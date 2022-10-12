c4bd9b63640cf6fb7c6c96f832acdd0575543b68
orlogue <102894674+orlogue@users.noreply.github.com>
2022-10-12 19:18:41 +1000

    Task 1: classes creation

---
+++ domain/Doctor.cs
@@ -0,0 +1,8 @@
+namespace domain;
+
+public class Doctor
+{
+    public int Id;
+    public string FullName;
+    public Specialty Speciality;
+}
---
+++ domain/User.cs
@@ -0,0 +1,9 @@
+namespace domain;
+
+public class User
+{
+    public int Id;
+    public string PhoneNumber;
+    public string FullName;
+    public Role Role;
+}
